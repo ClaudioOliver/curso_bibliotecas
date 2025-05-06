c0ecd679628f0fafea488a6c278bfcc6dbb5a017
ClaudioOliver <claudio.oliveira127@outlook.com>
2025-05-06 17:06:54 -0300

    Adiciona suporte para geração de chaves PIX
    - Referência ao namespace `bytebank_GeradorChavePix` no `Program.cs`.
    - Comentado o uso de `ByteBankAtendimento` e a classe `Estagiario`.
    - Implementação de geração e listagem de chaves PIX.
    - Atualização do arquivo `bytebank_ATENDIMENTO.csproj` para incluir a nova referência.

--- bytebank_ATENDIMENTO/Program.cs
+++ bytebank_ATENDIMENTO/Program.cs
@@ -1,24 +1,37 @@
 ﻿using bytebank.Modelos.ADM.Funcionarios;
 using bytebank_ATENDIMENTO.bytebank.Atendimento;
+using bytebank_GeradorChavePix;
 using bytebank_Modelos.bytebank.Modelos.ADM.Utilitario;
 
 Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
-new ByteBankAtendimento().AtendimentoCliente();
 
-public class Estagiario : Funcionario
+//new ByteBankAtendimento().AtendimentoCliente();
+
+#region Nivel de Visibilidade
+//public class Estagiario : Funcionario
+//{
+//    public Estagiario(double salario, string cpf) : base(salario, cpf)
+//    {
+//    }
+
+//    public override void AumentarSalario()
+//    {
+//        throw new NotImplementedException();
+//    }
+
+//    protected override double getBonificacao()
+//    {
+//        throw new NotImplementedException();
+//    }
+//}
+#endregion
+
+Console.WriteLine("Gera uma Chave PIX:");
+Console.WriteLine(GeradorPix.GetChavePix());
+
+Console.WriteLine("\nGera uma Listagem de Chaves PIX:");
+var listaDeChaves = GeradorPix.GetChavesPix(10);
+foreach (var chave in listaDeChaves)
 {
-    public Estagiario(double salario, string cpf) : base(salario, cpf)
-    {
-    }
-
-    public override void AumentarSalario()
-    {
-        throw new NotImplementedException();
-    }
-
-    protected override double getBonificacao()
-    {
-        throw new NotImplementedException();
-    }
+    Console.WriteLine(chave);
 }
-
