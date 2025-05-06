d3922e1a3371ba3fa86b6f2b7b7a05613acae67d
ClaudioOliver <claudio.oliveira127@outlook.com>
2025-05-06 12:24:15 -0300

    Adiciona classe Estagiario e refatora métodos de bonificação
    - Novas importações no `Program.cs` e inclusão da classe `Estagiario`.
    - Alterações nas assinaturas dos métodos `getBonificacao` para `protected internal` em várias classes.
    - Ajustes nos métodos `AumentarSalario` em `Auxiliar`, `Desenvolvedor`, `Designer`, `Diretor`, `GerenteDeContas` para diferentes percentuais de aumento.
    - Implementação da propriedade `autenticacao` em `FuncionarioAutenticavel` e `ParceiroComercial`, com modificação do método `Autenticar`.
    - Criação da classe `AutenticacaoUtil` para validação de senhas.

--- bytebank_Modelos/bytebank.Modelos/ADM/ParceriaComercial/ParceiroComercial.cs
+++ bytebank_Modelos/bytebank.Modelos/ADM/ParceriaComercial/ParceiroComercial.cs
@@ -1,13 +1,17 @@
 ﻿using bytebank.Modelos.ADM.SistemaInterno;
+using bytebank_Modelos.bytebank.Modelos.ADM.Utilitario;
 
 namespace bytebank.Modelos.ADM.Utilitario
 {
     public class ParceiroComercial : IAutenticavel
     {
         public string Senha { get; set; }
+
+        private AutenticacaoUtil autenticacao { get; set; }
+        
         public bool Autenticar(string senha)
         {
-            return Senha == senha;
+            return autenticacao.ValidarSenha(this.Senha, senha);
         }
     }
 }
